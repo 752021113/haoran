f449336602328ac857587fc6a6b2fdd30afbddbe
gggxxxx <100735184+gggxxxx@users.noreply.github.com>
2022-09-13 20:01:28 -0400

    Update classExercise.py
    I added the third numbers

--- classExercise.py
+++ classExercise.py
@@ -2,3 +2,7 @@ a = input("plz type the value of a: ")
 b = input("plz type the value of b: ")
 print("a+b= ",int(a)+int(b))
 
+#below are coded by Xiong Gu
+c = int(a) + int(b)
+d = 10
+print(c + d)
